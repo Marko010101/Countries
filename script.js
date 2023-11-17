44f5fb0b996ea1a3c67e4922df1b91ce2d057112
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-11-17 14:36:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -165,6 +165,10 @@ const getCountryAndNeighbours = function (countryCode) {
     });
 };
 
+btn.addEventListener('click', function () {
+  whereAmI();
+});
+
 ///////////////////////////////////////
 /// https://countries-api-836d.onrender.com/countries/
 // https://restcountries.eu/rest/v2/name/${country}`
@@ -271,32 +275,103 @@ const getCountryAndNeighbours = function (countryCode) {
     });
 }; */
 
-btn.addEventListener('click', function () {
-  whereAmI();
-  // getCountryData('sakartvelo');
-  // getCountryData('portugal');
+// console.log('test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('test end');
+
+/* const lottoryPromise = new Promise(function (resolve, reject) {
+  console.log('lotter draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💥'));
+    }
+  }, 2000);
 });
 
-/* 
-In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+lottoryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
-Here are your tasks:
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
-PART 1
-1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
-2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
-The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
-3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
-4. Chain a .catch method to the end of the promise chain and log errors to the console
-5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
 
-PART 2
-6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
-7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+Promise.resolve('abcs').then(x => console.log(x));
+Promise.reject('problem!').catch(x => console.error(x));
+ */
 
-TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
-TEST COORDINATES 2: 19.037, 72.873
-TEST COORDINATES 2: -33.933, 18.474
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
 
-GOOD LUCK 😀
-*/
+// getPosition().then(pos => console.log(pos));
+
+// const whereAmI = function () {
+//   if ('geolocation' in navigator) {
+//     getPosition()
+//       .then(pos => {
+//         const { latitude: lat, longitude: lng } = pos.coords;
+
+//         return fetch(
+//           `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+//         );
+//       })
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         const city = data.city;
+//         const country = data.countryName;
+//         const countryCode = data.countryCode;
+
+//         console.log(`You are in ${city}, ${country}`);
+
+//         return countryCode;
+//       })
+//       .then(countryCode => {
+//         // Add return here to chain the promise
+//         return getJSON(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+//       })
+//       .then(countries => {
+//         const country = countries[0];
+//         // Render the main country
+//         renderCountry(country);
+//         console.log(country);
+//         // Get the neighbor country data
+//         const neighbours = country.borders || [];
+//         console.log(neighbours);
+//         const neighbourPromises = neighbours.map(neighbour =>
+//           getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+//         );
+//         return Promise.all(neighbourPromises);
+//       })
+//       .then(neighbourDataArray => {
+//         // Render all the neighbor countries with the 'neighbour' class
+//         neighbourDataArray.forEach(neighbourData => {
+//           renderCountry(neighbourData[0], 'neighbour');
+//         });
+//       })
+//       .catch(error => {
+//         console.error('Error fetching location details:', error);
+//       })
+//       .finally(() => {
+//         countriesContainer.style.opacity = 1;
+//       });
+//   } else {
+//     console.error('Geolocation is not supported by your browser');
+//   }
+// };
+
+// btn.addEventListener('click', whereAmI);
