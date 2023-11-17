321a7318e3d5de3c9f59fb8d49109d9cd0d2824e
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-11-18 00:11:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,115 @@ const countriesContainer = document.querySelector('.countries');
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
 };
-const getJSON = function (url, errorMsg = 'Somthing went wrong') {
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
     if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
-    return response ? response.json() : null;
+    return response.json();
   });
 };
+
+const renderCountry = function (data, className = '') {
+  const flagSrc =
+    data.flags && (data.flags.png ? data.flags.png : data.flags.img);
+  countriesContainer.style.opacity = 1;
+
+  const html = `
+    <article class="country ${className}" data-country-code="${data.cca3}">
+      <img class="country__img" src="${flagSrc || ''}" />
+      <div class="country__data">
+        <h3 class="country__name">${
+          data.name && data.name.common ? data.name.common : 'N/A'
+        }</h3>
+        <h4 class="country__region">${data.region || 'N/A'}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${
+          data.languages ? Object.values(data.languages).join(', ') : 'N/A'
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data.currencies
+            ? Object.values(data.currencies)
+                .map(curr => curr.name)
+                .join(', ')
+            : 'N/A'
+        }</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  // Add click event listener to the country article
+  const countryArticle = countriesContainer.lastElementChild;
+  countryArticle.addEventListener('click', function () {
+    const countryCode = this.dataset.countryCode;
+    getCountryAndNeighbours(countryCode);
+  });
+};
+
+const getCountryAndNeighbours = function (countryCode) {
+  // Clear the countries container before rendering new countries
+  countriesContainer.innerHTML = '';
+
+  getJSON(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+    .then(country => {
+      // Render the clicked country
+      renderCountry(country[0], 'clicked');
+
+      // Get the neighbor country data
+      const neighbours = country[0].borders || [];
+
+      // Map each neighbour to a promise that fetches its data
+      const neighbourPromises = neighbours.map(neighbour =>
+        getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+      );
+
+      // Use Promise.all to wait for all neighbor data to be fetched
+      return Promise.all(neighbourPromises);
+    })
+    .then(neighbourDataArray => {
+      // Render all the neighbor countries with the 'neighbour' class
+      neighbourDataArray.forEach(neighbourData => {
+        renderCountry(neighbourData[0], 'neighbour');
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching country and neighbors:', error);
+    });
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+btn.addEventListener('click', async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse geocoding
+    const resGeo = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+    );
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+
+    const dataGeo = await resGeo.json();
+
+    // Call getCountryAndNeighbours with the country code
+    getCountryAndNeighbours(dataGeo.countryCode);
+  } catch (err) {
+    console.error(err);
+    renderError(`${err.message}`);
+    // Reject promise returned from async function
+    throw err;
+  }
+});
 
 // const getCountryData = function (country) {
 //   // Contry 1
@@ -92,77 +193,6 @@ const getJSON = function (url, errorMsg = 'Somthing went wrong') {
     console.error('Geolocation is not supported by your browser');
   }
 }; */
-
-const renderCountry = function (data, className = '') {
-  const flagSrc =
-    data.flags && (data.flags.png ? data.flags.png : data.flags.img);
-  countriesContainer.style.opacity = 1;
-
-  const html = `
-    <article class="country ${className}" data-country-code="${data.cca3}">
-      <img class="country__img" src="${flagSrc || ''}" />
-      <div class="country__data">
-        <h3 class="country__name">${
-          data.name && data.name.common ? data.name.common : 'N/A'
-        }</h3>
-        <h4 class="country__region">${data.region || 'N/A'}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} M</p>
-        <p class="country__row"><span>🗣️</span>${
-          data.languages ? Object.values(data.languages).join(', ') : 'N/A'
-        }</p>
-        <p class="country__row"><span>💰</span>${
-          data.currencies
-            ? Object.values(data.currencies)
-                .map(curr => curr.name)
-                .join(', ')
-            : 'N/A'
-        }</p>
-      </div>
-    </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-
-  // Add click event listener to the country article
-  const countryArticle = countriesContainer.lastElementChild;
-  countryArticle.addEventListener('click', function () {
-    const countryCode = this.dataset.countryCode;
-    getCountryAndNeighbours(countryCode);
-  });
-};
-
-const getCountryAndNeighbours = function (countryCode) {
-  // Clear the countries container before rendering new countries
-  countriesContainer.innerHTML = '';
-
-  getJSON(`https://restcountries.com/v3.1/alpha/${countryCode}`)
-    .then(country => {
-      // Render the clicked country
-      renderCountry(country[0], 'clicked');
-
-      // Get the neighbor country data
-      const neighbours = country[0].borders || [];
-
-      // Map each neighbour to a promise that fetches its data
-      const neighbourPromises = neighbours.map(neighbour =>
-        getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`)
-      );
-
-      // Use Promise.all to wait for all neighbor data to be fetched
-      return Promise.all(neighbourPromises);
-    })
-    .then(neighbourDataArray => {
-      // Render all the neighbor countries with the 'neighbour' class
-      neighbourDataArray.forEach(neighbourData => {
-        renderCountry(neighbourData[0], 'neighbour');
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching country and neighbors:', error);
-    });
-};
 
 ///////////////////////////////////////
 /// https://countries-api-836d.onrender.com/countries/
@@ -424,47 +454,6 @@ createImage('img/img-1.jpg')
   })
   .catch(err => console.error(err)); */
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
-
-const whereAmI = async function () {
-  try {
-    // Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
-
-    // Reverse geocoding
-    const resGeo = await fetch(
-      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-    );
-    if (!resGeo.ok) throw new Error('Problem getting location data');
-
-    const dataGeo = await resGeo.json();
-    console.log(dataGeo);
-
-    // Country data
-
-    const res = await fetch(
-      `https://restcountries.com/v3.1/name/${dataGeo.countryName}`
-    );
-    if (!res.ok) throw new Error('Problem getting country');
-
-    const data = await res.json();
-    renderCountry(data[0]);
-  } catch (err) {
-    console.error(err);
-    renderError(`${err.message}`);
-
-    // Reject promise returned from async function
-    throw err;
-  }
-};
-
-btn.addEventListener('click', whereAmI);
-
 // (async function () {
 //   try {
 //     const city = await whereAmI();
@@ -533,3 +522,84 @@ Promise.any([
   .then(res => console.log(res))
   .catch(err => console.error(err));
  */
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+// const wait = async function (seconds) {
+//   return await function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   };
+// };
+/* const imgContainer = document.querySelector('.images');
+
+const wait = async function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const loadNPause = async function (imagePath, paused) {
+  try {
+    const img = await createImage(imagePath);
+
+    await wait(paused);
+
+    img.style.display = 'none';
+    img.classList.add('paralell'); // Add the 'parallel' class to the image
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const createImage = async function (imagePath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imagePath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = await Promise.all(
+      imgArr.map(imgPath => loadNPause(imgPath, 2))
+    );
+    return imgs;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const allImage = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
+
+// Example usage
+loadAll(allImage).then(imgs => {
+  // imgs is an array of loaded images
+  console.log('All images loaded:', imgs);
+  console.log(
+    'Images with "parallel" class:',
+    document.querySelectorAll('.parallel')
+  );
+}); */
